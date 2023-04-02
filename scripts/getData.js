c1a1905963572a9c3971f6c1ab6f3c7a5da3936e
VadimHnatyuk28 <vadim.gnatyuk28@gmail.com>
2023-04-02 23:08:12 +0300

    bag

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const response = await fetch("./data/words.json");
+  const response = await fetch("words.json");
   const data = await response.json();
   console.log(data);
   return data;
